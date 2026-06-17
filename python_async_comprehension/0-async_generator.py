11f0017889998cadaa346ffff210ed5e2ff9ab47
David Lengellé <david.lengelle.1.10@gmail.com>
2026-06-17 18:08:26 +0200

    fix: add missing newline for better code readability in async_generator module

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -7,6 +7,7 @@ import random
 
 from typing import AsyncGenerator
 
+
 async def async_generator() -> AsyncGenerator[float, None]:
     """Coroutine yielding ten random floats between 0 and 10 every second"""
 
