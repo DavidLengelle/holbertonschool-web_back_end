1c5e3cb4e107570c7960a28e90cb2ced954bb4cb
David Lengellé <david.lengelle.1.10@gmail.com>
2026-06-17 18:07:27 +0200

    fix: remove unnecessary blank lines in async_generator module

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """Module defining an asynchronous generator of random numbers"""
 
+
 import asyncio
 import random
 
 from typing import AsyncGenerator
-
 
 async def async_generator() -> AsyncGenerator[float, None]:
     """Coroutine yielding ten random floats between 0 and 10 every second"""
