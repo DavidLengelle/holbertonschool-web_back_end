8d83d598eac689fe587cb617e617c8812fde6d66
David Lengellé <david.lengelle.1.10@gmail.com>
2026-06-17 10:39:03 +0200

    make 1-concurrent_coroutines.py executable and add module docstrings

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"""Coroutine that runs multiple wait_random coroutines concurrently"""
+
+
 import asyncio
 from typing import List
 
@@ -6,6 +9,8 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Spawn wait_random n times and return the delays in ascending order"""
+
     delays = []
     tasks = [wait_random(max_delay) for _ in range(n)]
     for task in asyncio.as_completed(tasks):
